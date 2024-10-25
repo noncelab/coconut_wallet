debb21183e3e9aea6261ce47fd865a666308f917
jay <jay@noncelab.com>
2024-10-25 10:31:24 +0900

    deploy: version 1.0.2
    - 버그수정
    - changelog 수정
    - ios 1.0.2+4
    - android 1.0.2+10

--- lib/constants/app_info.dart
+++ lib/constants/app_info.dart
@@ -1,5 +1,5 @@
 /// TODO: release date 기록
-const RELEASE_DATE = '2024.10.23';
+const RELEASE_DATE = '2024.10.25';
 const COPYRIGHT_TEXT =
     '© 2024 Nonce Lab. Inc. in S.Korea.\nLicensed under the MIT License with Commons Clause.';
 // const COPYRIGHT_TEXT = 'Made from Nonce Lab. Inc. in S.Korea.';
