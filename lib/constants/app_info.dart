0015adce830bda03f426b4e41219b3f2ef4bdc36
ella <eunji@noncelab.com>
2025-01-10 18:39:00 +0900

    deploy(2.1.1)

--- lib/constants/app_info.dart
+++ lib/constants/app_info.dart
@@ -1,5 +1,5 @@
 /// TODO: release date 기록
-const RELEASE_DATE = '2025.01.09';
+const RELEASE_DATE = '2025.01.10';
 const COPYRIGHT_TEXT =
     '© 2024 Nonce Lab. Inc. in S.Korea.\nLicensed under the MIT License with Commons Clause.';
 // const COPYRIGHT_TEXT = 'Made from Nonce Lab. Inc. in S.Korea.';
