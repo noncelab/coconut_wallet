6a6c28efc2f8620355435ba0bbe8203ae352b2c5
pio <pio@noncelab.com>
2025-01-13 16:57:56 +0900

    refactor(faucet-screen): 누락 UI 코드 추가
    - 앱바 영역의 패딩 수정
    - '받을 주소' 텍스트 복구
    #68

--- lib/screens/wallet_detail/faucet_request_screen.dart
+++ lib/screens/wallet_detail/faucet_request_screen.dart
@@ -33,133 +33,149 @@ class _FaucetRequestScreenState extends State<FaucetRequestScreen> {
           return GestureDetector(
             onTap: () => FocusScope.of(context).unfocus(),
             child: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 8, vertical: 8),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          IconButton(
+              child: Column(
+                children: [
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        IconButton(
+                          icon: const Icon(Icons.close, color: MyColors.white),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        ),
+                        const Text(
+                          '테스트 비트코인 받기',
+                          style: Styles.body1,
+                        ),
+                        Visibility(
+                          visible: false,
+                          maintainSize: true,
+                          maintainAnimation: true,
+                          maintainState: true,
+                          maintainSemantics: false,
+                          maintainInteractivity: false,
+                          child: IconButton(
                             icon:
                                 const Icon(Icons.close, color: MyColors.white),
                             onPressed: () {
                               Navigator.pop(context);
                             },
                           ),
-                          const Text(
-                            '테스트 비트코인 받기',
-                            style: Styles.body1,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16.0,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(height: 30),
+                        const Text(
+                          '받을 주소',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 16,
+                            color: MyColors.white,
+                            height: 1,
                           ),
-                          Visibility(
-                            visible: false,
-                            maintainSize: true,
-                            maintainAnimation: true,
-                            maintainState: true,
-                            maintainSemantics: false,
-                            maintainInteractivity: false,
-                            child: IconButton(
-                              icon: const Icon(Icons.close,
-                                  color: MyColors.white),
-                              onPressed: () {
-                                Navigator.pop(context);
-                              },
+                        ),
+                        const SizedBox(height: 10),
+                        CustomTextField(
+                            controller: viewModel.textController,
+                            placeholder:
+                                "주소를 입력해 주세요.\n주소는 [받기] 버튼을 눌러서 확인할 수 있어요.",
+                            onChanged: (text) {
+                              viewModel.validateAddress(text.toLowerCase());
+                            },
+                            maxLines: 2,
+                            style: Styles.body1.merge(TextStyle(
+                                fontFamily: CustomFonts.number.getFontFamily))),
+                        const SizedBox(height: 2),
+                        const SizedBox(height: 2),
+                        Visibility(
+                          visible: !viewModel.isErrorInAddress,
+                          maintainSize: true,
+                          maintainAnimation: true,
+                          maintainState: true,
+                          maintainSemantics: false,
+                          maintainInteractivity: false,
+                          child: Align(
+                            alignment: Alignment.centerRight,
+                            child: Text(
+                              '내 지갑(${viewModel.walletName}) 주소 - ${viewModel.walletIndex}',
+                              style: Styles.body2Number,
                             ),
                           ),
-                        ],
-                      ),
-                    ),
-                    const SizedBox(height: 30),
-                    CustomTextField(
-                        controller: viewModel.textController,
-                        placeholder:
-                            "주소를 입력해 주세요.\n주소는 [받기] 버튼을 눌러서 확인할 수 있어요.",
-                        onChanged: (text) {
-                          viewModel.validateAddress(text.toLowerCase());
-                        },
-                        maxLines: 2,
-                        style: Styles.body1.merge(TextStyle(
-                            fontFamily: CustomFonts.number.getFontFamily))),
-                    const SizedBox(height: 2),
-                    const SizedBox(height: 2),
-                    Visibility(
-                      visible: !viewModel.isErrorInAddress,
-                      maintainSize: true,
-                      maintainAnimation: true,
-                      maintainState: true,
-                      maintainSemantics: false,
-                      maintainInteractivity: false,
-                      child: Align(
-                        alignment: Alignment.centerRight,
-                        child: Text(
-                          '내 지갑(${viewModel.walletName}) 주소 - ${viewModel.walletIndex}',
-                          style: Styles.body2Number,
                         ),
-                      ),
+                      ],
                     ),
-                    const SizedBox(height: 24),
-                    IgnorePointer(
-                      ignoring: (viewModel.isErrorInAddress ||
-                          viewModel.isRequesting ||
-                          viewModel.isErrorInRemainingTime),
-                      child: CupertinoButton(
-                          onPressed: viewModel.canRequestFaucet
-                              ? () async {
-                                  if (viewModel.isLoading) {
-                                    CustomToast.showToast(
-                                        context: context,
-                                        text: '서버 상태를 확인하는 중입니다. 잠시만 기다려 주세요.');
-                                  }
-
-                                  if (viewModel.canRequestFaucet) {
-                                    await viewModel
-                                        .requestTestBitcoin((success, message) {
-                                      _onFaucetRequestResult(
-                                          context, success, message);
-                                      if (success) {
-                                        widget.onRequestSuccess!();
-                                      }
-                                    });
-                                    // await viewModel
-                                    //     .startFaucetRequest(widget.onRequestSuccess!);
-                                  }
-                                  FocusScope.of(context).unfocus();
+                  ),
+                  const SizedBox(height: 24),
+                  IgnorePointer(
+                    ignoring: (viewModel.isErrorInAddress ||
+                        viewModel.isRequesting ||
+                        viewModel.isErrorInRemainingTime),
+                    child: CupertinoButton(
+                        onPressed: viewModel.canRequestFaucet
+                            ? () async {
+                                if (viewModel.isLoading) {
+                                  CustomToast.showToast(
+                                      context: context,
+                                      text: '서버 상태를 확인하는 중입니다. 잠시만 기다려 주세요.');
                                 }
-                              : null,
-                          borderRadius: BorderRadius.circular(8.0),
-                          padding: EdgeInsets.zero,
-                          color: viewModel.canRequestFaucet
-                              ? MyColors.white
-                              : MyColors.transparentWhite_30,
-                          child: Container(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 28, vertical: 12),
-                              child: Text(
-                                viewModel.isRequesting
-                                    ? '요청 중...'
-                                    : '${formatNumber(viewModel.requestAmount)} BTC 요청하기',
-                                style: Styles.label.merge(TextStyle(
-                                    color: (viewModel.isErrorInAddress ||
-                                            viewModel.isRequesting ||
-                                            viewModel.isLoading)
-                                        ? MyColors.transparentBlack_50
-                                        : MyColors.black,
-                                    letterSpacing: -0.1,
-                                    fontWeight: FontWeight.w600)),
-                              ))),
-                    ),
-                    const SizedBox(height: 4),
-                    if (viewModel.isErrorInAddress) ...{
-                      _buildWarningMessage('올바른 주소인지 확인해 주세요'),
-                    } else if (viewModel.isErrorInRemainingTime) ...{
-                      _buildWarningMessage(
-                          '${viewModel.remainingTimeString} 후에 다시 시도해 주세요'),
-                    }
-                  ],
-                ),
+
+                                if (viewModel.canRequestFaucet) {
+                                  await viewModel
+                                      .requestTestBitcoin((success, message) {
+                                    _onFaucetRequestResult(
+                                        context, success, message);
+                                    if (success) {
+                                      widget.onRequestSuccess!();
+                                    }
+                                  });
+                                  // await viewModel
+                                  //     .startFaucetRequest(widget.onRequestSuccess!);
+                                }
+                                FocusScope.of(context).unfocus();
+                              }
+                            : null,
+                        borderRadius: BorderRadius.circular(8.0),
+                        padding: EdgeInsets.zero,
+                        color: viewModel.canRequestFaucet
+                            ? MyColors.white
+                            : MyColors.transparentWhite_30,
+                        child: Container(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 28, vertical: 12),
+                            child: Text(
+                              viewModel.isRequesting
+                                  ? '요청 중...'
+                                  : '${formatNumber(viewModel.requestAmount)} BTC 요청하기',
+                              style: Styles.label.merge(TextStyle(
+                                  color: (viewModel.isErrorInAddress ||
+                                          viewModel.isRequesting ||
+                                          viewModel.isLoading)
+                                      ? MyColors.transparentBlack_50
+                                      : MyColors.black,
+                                  letterSpacing: -0.1,
+                                  fontWeight: FontWeight.w600)),
+                            ))),
+                  ),
+                  const SizedBox(height: 4),
+                  if (viewModel.isErrorInAddress) ...{
+                    _buildWarningMessage('올바른 주소인지 확인해 주세요'),
+                  } else if (viewModel.isErrorInRemainingTime) ...{
+                    _buildWarningMessage(
+                        '${viewModel.remainingTimeString} 후에 다시 시도해 주세요'),
+                  }
+                ],
               ),
             ),
           );
