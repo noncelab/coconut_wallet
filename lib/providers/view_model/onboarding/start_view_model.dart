b2d1874cab07ebe425e29a0ea18189a12959f240
pio <pio@noncelab.com>
2025-01-17 17:37:22 +0900

    refactor(start-screen): mvvm 리팩토링
    - view와 관련없는 비즈니스 로직은 ViewModel로 이동
    #68

---
+++ lib/providers/view_model/onboarding/start_view_model.dart
@@ -0,0 +1,112 @@
+import 'dart:io';
+
+import 'package:coconut_wallet/app.dart';
+import 'package:coconut_wallet/constants/app_info.dart';
+import 'package:coconut_wallet/model/api/response/app_version_response.dart';
+import 'package:coconut_wallet/providers/app_sub_state_model.dart';
+import 'package:coconut_wallet/services/app_version_service.dart';
+import 'package:coconut_wallet/services/shared_prefs_service.dart';
+import 'package:coconut_wallet/utils/logger.dart';
+import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class StartViewModel extends ChangeNotifier {
+  /// Common variables ---------------------------------------------------------
+  final AppSubStateModel _subStateModel;
+  final SharedPrefs _sharedPrefs = SharedPrefs();
+  final AppVersion _appVersionRepository = AppVersion();
+
+  late PackageInfo _packageInfo;
+
+  bool _canUpdate = false;
+  bool _isLoading = true;
+
+  StartViewModel(this._subStateModel) {
+    _initialize();
+  }
+
+  bool get canUpdate => _canUpdate;
+  bool get isLoading => _isLoading;
+
+  /// 시작 화면 결정
+  Future<AccessFlow> determineStartScreen() async {
+    // Splash 보여주기 위한 딜레이
+    await Future.delayed(const Duration(seconds: 2));
+    await _subStateModel.setInitData();
+
+    if (!_subStateModel.hasLaunchedBefore) {
+      await _subStateModel.setHasLaunchedBefore();
+    }
+
+    if (!_subStateModel.isNotEmptyWalletList || !_subStateModel.isSetPin) {
+      return AccessFlow.main;
+    } else {
+      return AccessFlow.pinCheck;
+    }
+  }
+
+  /// 업데이트 실행
+  Future<void> launchUpdate() async {
+    Uri storeUrl = Platform.isAndroid
+        ? Uri.parse(
+            'https://play.google.com/store/apps/details?id=${_packageInfo.packageName}')
+        : Uri.parse('https://apps.apple.com/kr/app/$APPSTORE_ID');
+
+    if (await canLaunchUrl(storeUrl)) {
+      await launchUrl(storeUrl);
+    }
+  }
+
+  /// 업데이트 다이얼로그를 마지막으로 띄운 시간 저장
+  Future<void> setNextUpdateDialogDate() async {
+    final nextShowDate = DateTime.now().add(const Duration(days: 7));
+    _sharedPrefs.setString(SharedPrefs.kNextVersionUpdateDialogDate,
+        nextShowDate.toIso8601String());
+  }
+
+  /// 앱 최신 버전 확인
+  Future<void> _checkLatestVersion() async {
+    try {
+      final response = await _appVersionRepository.getLatestAppVersion();
+      if (response is AppVersionResponse) {
+        String currentVersion = _packageInfo.version;
+        String newVersion = response.latestVersion;
+        Logger.log(
+            'currentVersion : $currentVersion\nnewVersion : $newVersion');
+        if (currentVersion.isNotEmpty && newVersion.isNotEmpty) {
+          // 메이저 버전이 다를 경우만 판별
+          if (int.tryParse(currentVersion.split('.')[0])! <
+              int.tryParse(newVersion.split('.')[0])!) {
+            _canUpdate = await _shouldShowUpdateDialog();
+          }
+        }
+      }
+    } catch (e) {
+      Logger.log('[앱 버전 체크 ERROR] ${e.toString()}');
+    }
+  }
+
+  /// 초기화
+  Future<void> _initialize() async {
+    await _initPackageInfo();
+    await _checkLatestVersion();
+    _isLoading = false;
+    notifyListeners();
+  }
+
+  Future<void> _initPackageInfo() async {
+    _packageInfo = await PackageInfo.fromPlatform();
+  }
+
+  /// 업데이트 다이얼로그 표시 여부 결정
+  Future<bool> _shouldShowUpdateDialog() async {
+    final nextShowDateString =
+        _sharedPrefs.getString(SharedPrefs.kNextVersionUpdateDialogDate);
+
+    if (nextShowDateString.isEmpty) return true;
+
+    final nextShowDate = DateTime.parse(nextShowDateString);
+    return DateTime.now().isAfter(nextShowDate);
+  }
+}
--- lib/screens/onboarding/start_screen.dart
+++ lib/screens/onboarding/start_screen.dart
@@ -1,19 +1,12 @@
 import 'dart:async';
-import 'dart:io';
 
 import 'package:coconut_wallet/app.dart';
-import 'package:coconut_wallet/constants/app_info.dart';
+import 'package:coconut_wallet/providers/view_model/onboarding/start_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:coconut_wallet/providers/app_sub_state_model.dart';
 import 'package:coconut_wallet/styles.dart';
-import 'package:package_info_plus/package_info_plus.dart';
-import 'package:coconut_wallet/model/api/response/app_version_response.dart';
-import 'package:coconut_wallet/services/app_version_service.dart';
-import 'package:coconut_wallet/services/shared_prefs_service.dart';
-import 'package:coconut_wallet/utils/logger.dart';
 import 'package:coconut_wallet/widgets/custom_dialogs.dart';
 import 'package:provider/provider.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class StartScreen extends StatefulWidget {
   final void Function(AccessFlow nextScreen) onComplete;
@@ -25,76 +18,28 @@ class StartScreen extends StatefulWidget {
 }
 
 class _StartScreenState extends State<StartScreen> {
-  late AppSubStateModel _subModel;
-  final AppVersion _repository = AppVersion();
-  bool canUpdate = false;
-  late PackageInfo _packageInfo;
-  final SharedPrefs _sharedPrefs = SharedPrefs();
+  late StartViewModel _viewModel;
 
   @override
   void initState() {
     super.initState();
-    _subModel = Provider.of<AppSubStateModel>(context, listen: false);
+    _viewModel =
+        StartViewModel(Provider.of<AppSubStateModel>(context, listen: false));
+
     _initialize();
   }
 
   void _initialize() async {
-    await _initPackageInfo();
-    setStartScreen();
-  }
-
-  Future<void> _initPackageInfo() async {
-    _packageInfo = await PackageInfo.fromPlatform();
-  }
-
-  /// 앱 시작 후 어떤 화면을 최초로 보여줄 지 결정합니다.
-  void setStartScreen() async {
-    await _checkLatestVersion();
     await Future.delayed(const Duration(seconds: 1));
-    if (canUpdate) {
+    if (_viewModel.canUpdate) {
       bool finishDialogValue = await _showUpdateDialog();
       if (!finishDialogValue) {
-        await _setNextUpdateDialogDate();
+        await _viewModel.setNextUpdateDialogDate();
       }
     }
-    _loadScreenStatus();
-  }
 
-  void _loadScreenStatus() async {
-    // Splash 보여주기 위한 딜레이
-    await Future.delayed(const Duration(seconds: 2));
-    await _subModel.setInitData();
-    if (!_subModel.hasLaunchedBefore) {
-      await _subModel.setHasLaunchedBefore();
-    }
-
-    if (!_subModel.isNotEmptyWalletList || !_subModel.isSetPin) {
-      widget.onComplete(AccessFlow.main);
-    } else {
-      widget.onComplete(AccessFlow.pinCheck);
-    }
-  }
-
-  /// 앱 최신버전 조회
-  Future<void> _checkLatestVersion() async {
-    try {
-      final response = await _repository.getLatestAppVersion();
-      if (response is AppVersionResponse) {
-        String currentVersion = _packageInfo.version;
-        String newVersion = response.latestVersion;
-        Logger.log(
-            'currentVersion : $currentVersion\nnewVersion : $newVersion');
-        if (currentVersion.isNotEmpty && newVersion.isNotEmpty) {
-          // 메이저 버전이 다를 경우만 판별
-          if (int.tryParse(currentVersion.split('.')[0])! <
-              int.tryParse(newVersion.split('.')[0])!) {
-            canUpdate = await _shouldShowUpdateDialog();
-          }
-        }
-      }
-    } catch (e) {
-      Logger.log('[앱 버전 체크 ERROR] ${e.toString()}');
-    }
+    AccessFlow nextScreen = await _viewModel.determineStartScreen();
+    widget.onComplete(nextScreen);
   }
 
   /// 업데이트 다이얼로그 표시
@@ -104,50 +49,17 @@ class _StartScreenState extends State<StartScreen> {
           title: '업데이트 알림',
           message: '안정적인 서비스 이용을 위해\n최신 버전으로 업데이트 해주세요.',
           onConfirm: () async {
-            Uri storeUrl = Platform.isAndroid
-                ? Uri.parse(
-                    'https://play.google.com/store/apps/details?id=${_packageInfo.packageName}')
-                : Uri.parse('https://apps.apple.com/kr/app/$APPSTORE_ID');
-
-            if (await canLaunchUrl(storeUrl)) {
-              await launchUrl(storeUrl);
-            }
-
-            return true; // 업데이트 버튼을 눌렀을 때 true 반환
+            await _viewModel.launchUpdate();
+            return true;
           },
           onCancel: () {
-            return false; // 다음에 하기 버튼을 눌렀을 때 false 반환
+            return false;
           },
           confirmButtonText: '업데이트하기',
           confirmButtonColor: MyColors.primary,
           cancelButtonText: '다음에 하기',
         )) ??
         false;
-  }
-
-  /// 다이얼로그를 마지막으로 띄운 시간을 저장
-  Future<void> _setNextUpdateDialogDate() async {
-    final nextShowDate = DateTime.now().add(const Duration(days: 7));
-    _sharedPrefs.setString(SharedPrefs.kNextVersionUpdateDialogDate,
-        nextShowDate.toIso8601String());
-  }
-
-  /// 다이얼로그를 띄울지 여부 결졍
-  Future<bool> _shouldShowUpdateDialog() async {
-    final nextShowDateString =
-        SharedPrefs().getString(SharedPrefs.kNextVersionUpdateDialogDate);
-
-    if (nextShowDateString.isEmpty) {
-      return true;
-    }
-    final nextShowDate = DateTime.parse(nextShowDateString);
-    if (DateTime.now().isAfter(nextShowDate)) {
-      return true;
-    }
-
-    Logger.log(
-        'User already Canceled Update Dialog. The Next Show Dialog Date is : $nextShowDate');
-    return false;
   }
 
   @override
