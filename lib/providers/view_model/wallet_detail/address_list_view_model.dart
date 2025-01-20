aa3294ce4f87f8f6de20a447c1e19e2ba843a56a
pio <pio@noncelab.com>
2025-01-20 13:47:38 +0900

    refactor(address-list): mvvm 리팩토링
    - view에서는 페이징, 툴팁, 스크롤 관련 데이터만 유지
    - 나머지 wallet관련 데이터는 viewModel로 분리
    #68

---
+++ lib/providers/view_model/wallet_detail/address_list_view_model.dart
@@ -0,0 +1,33 @@
+import 'package:coconut_lib/coconut_lib.dart';
+import 'package:coconut_wallet/model/app/wallet/wallet_list_item_base.dart';
+import 'package:coconut_wallet/providers/app_state_model.dart';
+import 'package:flutter/material.dart';
+
+class AddressListViewModel extends ChangeNotifier {
+  /// Common variables ---------------------------------------------------------
+  late final AppStateModel _appStateModel;
+
+  /// Wallet variables ---------------------------------------------------------
+  List<Address>? _receivingAddressList;
+  List<Address>? get receivingAddressList => _receivingAddressList;
+  List<Address>? _changeAddressList;
+  List<Address>? get changeAddressList => _changeAddressList;
+
+  WalletBase? _walletBase;
+  WalletBase? get walletBase => _walletBase;
+  WalletListItemBase? _walletBaseItem;
+  WalletListItemBase? get walletBaseItem => _walletBaseItem;
+
+  AddressListViewModel(this._appStateModel, int id, int firstCount) {
+    _initialize(id, firstCount);
+  }
+
+  /// 초기화
+  Future<void> _initialize(int id, int firstCount) async {
+    _walletBaseItem = _appStateModel.getWalletById(id);
+    _walletBase = _walletBaseItem!.walletBase;
+    _receivingAddressList = _walletBase?.getAddressList(0, firstCount, false);
+    _changeAddressList = _walletBase?.getAddressList(0, firstCount, true);
+    notifyListeners();
+  }
+}
