b3e244ca308104fa831a72b8deee026395bef6d0
pio <pio@noncelab.com>
2025-01-23 17:04:14 +0900

    refactor(send-broadcasting): viewModel 추가 분리
    - sort members
    - setTxInfo 메서드 viewModel로 이동
    
    Co-Authored-By: EllaLim <73803777+Ella-noncelab@users.noreply.github.com>
    #68

--- lib/screens/send/broadcasting_screen.dart
+++ lib/screens/send/broadcasting_screen.dart
@@ -1,13 +1,11 @@
 import 'package:coconut_lib/coconut_lib.dart';
 import 'package:coconut_wallet/enums/currency_enums.dart';
+import 'package:coconut_wallet/model/app/error/app_error.dart';
 import 'package:coconut_wallet/providers/connectivity_provider.dart';
 import 'package:coconut_wallet/providers/send_info_provider.dart';
 import 'package:coconut_wallet/providers/upbit_connect_model.dart';
 import 'package:coconut_wallet/providers/view_model/send/broadcasting_view_model.dart';
 import 'package:coconut_wallet/providers/wallet_provider.dart';
-import 'package:flutter/material.dart';
-import 'package:loader_overlay/loader_overlay.dart';
-import 'package:coconut_wallet/model/app/error/app_error.dart';
 import 'package:coconut_wallet/styles.dart';
 import 'package:coconut_wallet/utils/alert_util.dart';
 import 'package:coconut_wallet/utils/balance_format_util.dart';
@@ -15,8 +13,10 @@ import 'package:coconut_wallet/utils/fiat_util.dart';
 import 'package:coconut_wallet/utils/logger.dart';
 import 'package:coconut_wallet/utils/vibration_util.dart';
 import 'package:coconut_wallet/widgets/appbar/custom_appbar.dart';
-import 'package:coconut_wallet/widgets/custom_toast.dart';
 import 'package:coconut_wallet/widgets/card/information_item_card.dart';
+import 'package:coconut_wallet/widgets/custom_toast.dart';
+import 'package:flutter/material.dart';
+import 'package:loader_overlay/loader_overlay.dart';
 import 'package:provider/provider.dart';
 
 class BroadcastingScreen extends StatefulWidget {
@@ -27,95 +27,7 @@ class BroadcastingScreen extends StatefulWidget {
 }
 
 class _BroadcastingScreenState extends State<BroadcastingScreen> {
-  String? _address;
-  int? _amount;
-  int? _fee;
-  int? _totalAmount;
-  bool _initDone = false;
-  bool _isValidSignedTransaction = false;
-  int? _sendingAmountWhenAddressIsMyChange; // 내 지갑의 change address로 보내는 경우 잔액
-  bool _isSendingToMyAddress = false;
-  List<int> outputIndexesToMyAddress = [];
   late BroadcastingViewModel _viewModel;
-
-  @override
-  void initState() {
-    super.initState();
-    _viewModel = BroadcastingViewModel(
-        Provider.of<SendInfoProvider>(context, listen: false),
-        Provider.of<WalletProvider>(context, listen: false),
-        Provider.of<ConnectivityProvider>(context, listen: false).isNetworkOn);
-
-    WidgetsBinding.instance.addPostFrameCallback((duration) {
-      setOverlayLoading(true);
-      setTxInfo();
-      setOverlayLoading(false);
-    });
-  }
-
-  void setTxInfo() async {
-    try {
-      PSBT signedPsbt = PSBT.parse(_viewModel.signedTransaction);
-      // print("!!! -> ${_model.signedTransaction!}");
-      List<PsbtOutput> outputs = signedPsbt.outputs;
-
-      // case1. 다른 사람에게 보내고(B1) 잔액이 있는 경우(A2)
-      // case2. 다른 사람에게 보내고(B1) 잔액이 없는 경우
-      // case3. 내 지갑의 다른 주소로 보내고(A2) 잔액이 있는 경우(A3)
-      // case4. 내 지갑의 다른 주소로 보내고(A2) 잔액이 없는 경우
-      // 만약 실수로 내 지갑의 change address로 보내는 경우에는 sendingAmount가 0
-      List<PsbtOutput> outputToMyReceivingAddress = [];
-      List<PsbtOutput> outputToMyChangeAddress = [];
-      List<PsbtOutput> outputsToOther = [];
-      for (int i = 0; i < outputs.length; i++) {
-        if (outputs[i].derivationPath == null) {
-          outputsToOther.add(outputs[i]);
-        } else if (outputs[i].isChange) {
-          outputToMyChangeAddress.add(outputs[i]);
-          outputIndexesToMyAddress.add(i);
-        } else {
-          outputToMyReceivingAddress.add(outputs[i]);
-          outputIndexesToMyAddress.add(i);
-        }
-      }
-
-      PsbtOutput? output;
-      if (outputsToOther.isNotEmpty) {
-        setState(() {
-          output = outputsToOther[0];
-        });
-      } else if (outputToMyReceivingAddress.isNotEmpty) {
-        setState(() {
-          output = outputToMyReceivingAddress[0];
-          _isSendingToMyAddress = true;
-        });
-      } else if (outputToMyChangeAddress.isNotEmpty) {
-        // 받는 주소에 내 지갑의 change address를 입력한 경우
-        // 원래 이 경우 output.sendingAmount = 0, 보낼 주소가 표기되지 않았었지만, 버그처럼 보이는 문제 때문에 대응합니다.
-        // (주의!!) coconut_lib에서 output 배열에 sendingOutput을 먼저 담으므로 항상 첫번째 것을 사용하면 전액 보내기일 때와 아닐 때 모두 커버 됨
-        // 하지만 coconut_lib에 종속적이므로 coconut_lib에 변경 발생 시 대응 필요
-        setState(() {
-          output = outputToMyChangeAddress[0];
-          _sendingAmountWhenAddressIsMyChange = output!.amount;
-          _isSendingToMyAddress = true;
-        });
-      }
-
-      setState(() {
-        _address = output?.getAddress() ?? '';
-        _amount = signedPsbt.sendingAmount;
-
-        _fee = signedPsbt.fee;
-        _initDone = true;
-        _totalAmount = signedPsbt.sendingAmount + signedPsbt.fee;
-
-        _isValidSignedTransaction = true;
-      });
-    } catch (e) {
-      vibrateMedium();
-      showAlertDialog(context: context, content: "트랜잭션 파싱 실패: $e");
-    }
-  }
 
   void broadcast() async {
     setOverlayLoading(true);
@@ -131,6 +43,7 @@ class _BroadcastingScreenState extends State<BroadcastingScreen> {
 
       if (result.isFailure) {
         vibrateMedium();
+        if (!mounted) return;
         showAlertDialog(
             context: context, content: "전송 실패\n${result.error?.message}");
         return;
@@ -147,7 +60,7 @@ class _BroadcastingScreenState extends State<BroadcastingScreen> {
         //         .toList()
         //     : [];
         // await _model.updateTagsOfUsedUtxos(widget.id, newUtxoIds);
-
+        if (!mounted) return;
         Navigator.pushNamedAndRemoveUntil(
           context,
           '/broadcasting-complete', // 이동할 경로
@@ -166,16 +79,9 @@ class _BroadcastingScreenState extends State<BroadcastingScreen> {
       if (_.toString().contains('min relay fee not met')) {
         message = '[전송 실패]\n수수료율을 높여서\n다시 시도해주세요.';
       }
+      if (!mounted) return;
       showAlertDialog(context: context, content: message);
       vibrateMedium();
-    }
-  }
-
-  void setOverlayLoading(bool value) {
-    if (value) {
-      context.loaderOverlay.show();
-    } else {
-      context.loaderOverlay.hide();
     }
   }
 
@@ -199,7 +105,7 @@ class _BroadcastingScreenState extends State<BroadcastingScreen> {
           appBar: CustomAppBar.buildWithNext(
               title: '최종 확인',
               context: context,
-              isActive: _isValidSignedTransaction,
+              isActive: _viewModel.isValidSignedTransaction,
               onNextPressed: () {
                 if (viewModel.isNetworkOn == false) {
                   CustomToast.showWarningToast(
@@ -207,7 +113,7 @@ class _BroadcastingScreenState extends State<BroadcastingScreen> {
                   return;
                 }
 
-                if (_initDone) {
+                if (_viewModel.isInitDone) {
                   broadcast();
                 }
               }),
@@ -232,12 +138,12 @@ class _BroadcastingScreenState extends State<BroadcastingScreen> {
                         child: Center(
                           child: Text.rich(
                             TextSpan(
-                                text: _amount != null
-                                    ? satoshiToBitcoinString(
-                                        _sendingAmountWhenAddressIsMyChange !=
-                                                null
-                                            ? _sendingAmountWhenAddressIsMyChange!
-                                            : _amount!)
+                                text: _viewModel.amount != null
+                                    ? satoshiToBitcoinString(_viewModel
+                                                .sendingAmountWhenAddressIsMyChange !=
+                                            null
+                                        ? _viewModel.sendingAmountWhenAddressIsMyChange!
+                                        : _viewModel.amount!)
                                     : "",
                                 children: const <TextSpan>[
                                   TextSpan(text: ' BTC', style: Styles.unit)
@@ -248,13 +154,15 @@ class _BroadcastingScreenState extends State<BroadcastingScreen> {
                     Selector<UpbitConnectModel, int?>(
                       selector: (context, model) => model.bitcoinPriceKrw,
                       builder: (context, bitcoinPriceKrw, child) {
-                        if (bitcoinPriceKrw != null && _amount != null) {
+                        if (bitcoinPriceKrw != null &&
+                            _viewModel.amount != null) {
                           String bitcoinPriceKrwString = addCommasToIntegerPart(
                               FiatUtil.calculateFiatAmount(
-                                      _sendingAmountWhenAddressIsMyChange !=
+                                      _viewModel.sendingAmountWhenAddressIsMyChange !=
                                               null
-                                          ? _sendingAmountWhenAddressIsMyChange!
-                                          : _amount!,
+                                          ? _viewModel
+                                              .sendingAmountWhenAddressIsMyChange!
+                                          : _viewModel.amount!,
                                       bitcoinPriceKrw)
                                   .toDouble());
                           return Text(
@@ -284,15 +192,15 @@ class _BroadcastingScreenState extends State<BroadcastingScreen> {
                                 children: [
                                   InformationItemCard(
                                       label: '보낼 주소',
-                                      value: _address ?? "",
+                                      value: _viewModel.address ?? "",
                                       isNumber: true),
                                   const Divider(
                                       color: MyColors.transparentWhite_12,
                                       height: 1),
                                   InformationItemCard(
                                       label: '예상 수수료',
-                                      value: _fee != null
-                                          ? "${satoshiToBitcoinString(_fee!)} BTC"
+                                      value: _viewModel.fee != null
+                                          ? "${satoshiToBitcoinString(_viewModel.fee!)} BTC"
                                           : '',
                                       isNumber: true),
                                   const Divider(
@@ -300,14 +208,14 @@ class _BroadcastingScreenState extends State<BroadcastingScreen> {
                                       height: 1),
                                   InformationItemCard(
                                       label: '총 소요 수량',
-                                      value: _totalAmount != null
-                                          ? "${satoshiToBitcoinString(_totalAmount!)} BTC"
+                                      value: _viewModel.totalAmount != null
+                                          ? "${satoshiToBitcoinString(_viewModel.totalAmount!)} BTC"
                                           : '',
                                       isNumber: true),
                                 ],
                               ))),
                     ),
-                    if (_isSendingToMyAddress) ...[
+                    if (_viewModel.isSendingToMyAddress) ...[
                       const SizedBox(
                         height: 20,
                       ),
@@ -325,5 +233,35 @@ class _BroadcastingScreenState extends State<BroadcastingScreen> {
         ),
       ),
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _viewModel = BroadcastingViewModel(
+        Provider.of<SendInfoProvider>(context, listen: false),
+        Provider.of<WalletProvider>(context, listen: false),
+        Provider.of<ConnectivityProvider>(context, listen: false).isNetworkOn);
+
+    WidgetsBinding.instance.addPostFrameCallback((duration) {
+      setOverlayLoading(true);
+
+      try {
+        _viewModel.setTxInfo();
+      } catch (e) {
+        vibrateMedium();
+        showAlertDialog(context: context, content: "트랜잭션 파싱 실패: $e");
+      }
+
+      setOverlayLoading(false);
+    });
+  }
+
+  void setOverlayLoading(bool value) {
+    if (value) {
+      context.loaderOverlay.show();
+    } else {
+      context.loaderOverlay.hide();
+    }
   }
 }
