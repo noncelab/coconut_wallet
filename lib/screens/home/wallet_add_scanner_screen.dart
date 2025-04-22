fbd3eb2b4a97b734724eb01132d7a32b6c8bd5bc
pio <pio@noncelab.com>
2025-04-22 18:29:31 +0900

    feat(add-scanner-guide): 선택한 지갑 추가 기기에 따라 스캔 화면에서 Appbar title, guide 문구 변경
    #179

--- lib/screens/home/wallet_add_scanner_screen.dart
+++ lib/screens/home/wallet_add_scanner_screen.dart
@@ -15,7 +15,6 @@ import 'package:coconut_wallet/utils/vibration_util.dart';
 import 'package:coconut_wallet/widgets/custom_dialogs.dart';
 import 'package:provider/provider.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
-import '../../widgets/appbar/custom_appbar.dart';
 
 class WalletAddScannerScreen extends StatefulWidget {
   final WalletImportSource walletImportSource;
@@ -33,6 +32,7 @@ class _WalletAddScannerScreenState extends State<WalletAddScannerScreen> {
   Barcode? result;
   QRViewController? controller;
   bool _isProcessing = false;
+
   // In order to get hot reload to work we need to pause the camera if the platform
   // is android, or resume the camera if the platform is iOS.
   @override
@@ -45,27 +45,91 @@ class _WalletAddScannerScreenState extends State<WalletAddScannerScreen> {
     }
   }
 
+  List<TextSpan> _getGuideTextSpan() {
+    switch (widget.walletImportSource) {
+      case WalletImportSource.coconutVault:
+        {
+          return [
+            TextSpan(
+              text: t.wallet_add_scanner_screen.guide_vault,
+            ),
+          ];
+        }
+      case WalletImportSource.keystone:
+        {
+          return [
+            TextSpan(text: t.wallet_add_scanner_screen.guide_keystone.step1),
+            _em(t.wallet_add_scanner_screen.guide_keystone.step1_em),
+            TextSpan(text: t.wallet_add_scanner_screen.select),
+            const TextSpan(text: '\n'),
+            TextSpan(text: t.wallet_add_scanner_screen.guide_keystone.step2),
+            _em(t.wallet_add_scanner_screen.guide_keystone.step2_em),
+            TextSpan(text: t.wallet_add_scanner_screen.select),
+            const TextSpan(text: '\n'),
+            TextSpan(text: t.wallet_add_scanner_screen.guide_keystone.step3),
+            _em(t.wallet_add_scanner_screen.guide_keystone.step3_em),
+            TextSpan(text: t.wallet_add_scanner_screen.select),
+            const TextSpan(text: '\n'),
+            TextSpan(text: t.wallet_add_scanner_screen.guide_keystone.step4),
+            _em(t.wallet_add_scanner_screen.guide_keystone.step4_em1),
+            TextSpan(text: t.wallet_add_scanner_screen.guide_keystone.next),
+            _em(t.wallet_add_scanner_screen.guide_keystone.step4_em2),
+            TextSpan(text: t.wallet_add_scanner_screen.select),
+            const TextSpan(text: '\n'),
+            TextSpan(text: t.wallet_add_scanner_screen.guide_keystone.step5),
+            _em(t.wallet_add_scanner_screen.guide_keystone.step5_em),
+            const TextSpan(text: '\n'),
+            TextSpan(text: t.wallet_add_scanner_screen.guide_keystone.step6),
+            _em(t.wallet_add_scanner_screen.guide_keystone.step6_em),
+            TextSpan(text: t.wallet_add_scanner_screen.guide_keystone.step6_end),
+          ];
+        }
+      case WalletImportSource.seedSigner:
+        {
+          return [
+            TextSpan(text: t.wallet_add_scanner_screen.guide_seedsigner.step1),
+            _em(t.wallet_add_scanner_screen.guide_seedsigner.step1_em),
+            TextSpan(text: t.wallet_add_scanner_screen.select),
+            const TextSpan(text: '\n'),
+            TextSpan(text: t.wallet_add_scanner_screen.guide_seedsigner.step2),
+            _em(t.wallet_add_scanner_screen.guide_seedsigner.step2_em),
+            TextSpan(text: t.wallet_add_scanner_screen.select),
+            const TextSpan(text: '\n'),
+            TextSpan(text: t.wallet_add_scanner_screen.guide_seedsigner.step3),
+          ];
+        }
+      default:
+        return [];
+    }
+  }
+
+  TextSpan _em(String text) => TextSpan(
+        text: text,
+        style: CoconutTypography.body3_12_Bold,
+      );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: CoconutAppBar.build(
-          title: t.wallet_add_scanner_screen.add_watch_only_wallet,
-          context: context,
-          isBottom: true,
-          backgroundColor: CoconutColors.black.withOpacity(0.95),
-          actionButtonList: [
-            IconButton(
-              onPressed: () {
-                if (controller != null) {
-                  controller!.flipCamera();
-                }
-              },
-              icon: const Icon(CupertinoIcons.camera_rotate, size: 22),
-              color: CoconutColors.white,
-            ),
-          ],
-        ),
-        body: Stack(children: [
+      appBar: CoconutAppBar.build(
+        title: _getAppBarTitle(),
+        context: context,
+        isBottom: true,
+        backgroundColor: CoconutColors.black.withOpacity(0.95),
+        actionButtonList: [
+          IconButton(
+            onPressed: () {
+              if (controller != null) {
+                controller!.flipCamera();
+              }
+            },
+            icon: const Icon(CupertinoIcons.camera_rotate, size: 22),
+            color: CoconutColors.white,
+          ),
+        ],
+      ),
+      body: Stack(
+        children: [
           Container(
             color: CoconutColors.black,
             child: Column(
@@ -93,52 +157,39 @@ class _WalletAddScannerScreenState extends State<WalletAddScannerScreen> {
             ),
           ),
           Padding(
-              padding: const EdgeInsets.only(
-                  top: 20, left: CoconutLayout.defaultPadding, right: CoconutLayout.defaultPadding),
-              child: CoconutToolTip(
-                  baseBackgroundColor: CoconutColors.white.withOpacity(0.95),
-                  tooltipType: CoconutTooltipType.fixed,
-                  richText: RichText(
-                    text: TextSpan(
-                      text: t.tooltip.wallet_add1,
-                      style: const TextStyle(
-                        fontFamily: 'Pretendard',
-                        fontWeight: FontWeight.normal,
-                        fontSize: 15,
-                        height: 1.4,
-                        letterSpacing: 0.5,
-                        color: CoconutColors.black,
-                      ),
-                      children: <TextSpan>[
-                        TextSpan(
-                          text: t.tooltip.wallet_add2,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        TextSpan(
-                          text: t.tooltip.wallet_add3,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.normal,
-                          ),
-                        ),
-                        TextSpan(
-                          text: t.tooltip.wallet_add4,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        TextSpan(
-                          text: t.tooltip.wallet_add5,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.normal,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ))),
-        ]));
+            padding: const EdgeInsets.only(
+                top: 20, left: CoconutLayout.defaultPadding, right: CoconutLayout.defaultPadding),
+            child: CoconutToolTip(
+              backgroundColor: CoconutColors.gray900,
+              borderColor: CoconutColors.gray900,
+              // TODO: CDS #20 머지 후 주석 삭제
+              // icon: SvgPicture.asset(
+              //   'packages/coconut_design_system/assets/svg/info_circle.svg',
+              //   colorFilter: const ColorFilter.mode(
+              //     CoconutColors.white,
+              //     BlendMode.srcIn,
+              //   ),
+              // ),
+              tooltipType: CoconutTooltipType.fixed,
+              richText: RichText(
+                text: TextSpan(
+                  style: CoconutTypography.body3_12,
+                  children: _getGuideTextSpan(),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
+
+  String _getAppBarTitle() => switch (widget.walletImportSource) {
+        WalletImportSource.coconutVault => t.wallet_add_scanner_screen.vault,
+        WalletImportSource.keystone => t.wallet_add_scanner_screen.keystone,
+        WalletImportSource.seedSigner => t.wallet_add_scanner_screen.seed_signer,
+        _ => '',
+      };
 
   void _onQRViewCreated(QRViewController controller) {
     this.controller = controller;
