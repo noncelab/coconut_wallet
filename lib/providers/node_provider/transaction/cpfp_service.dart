9baf6a02c44b0ded00cd6685ffb81f07d7d5ea20
tiger <tiger@noncelab.com>
2025-04-29 15:37:53 +0900

    refactor(cpfp): CpfpService 리팩토링, 테스트 코드 작성

--- lib/providers/node_provider/transaction/cpfp_service.dart
+++ lib/providers/node_provider/transaction/cpfp_service.dart
@@ -1,4 +1,5 @@
 import 'package:coconut_lib/coconut_lib.dart';
+import 'package:coconut_wallet/model/utxo/utxo_state.dart';
 import 'package:coconut_wallet/model/wallet/transaction_record.dart';
 import 'package:coconut_wallet/model/wallet/wallet_list_item_base.dart';
 import 'package:coconut_wallet/repository/realm/transaction_repository.dart';
@@ -8,8 +9,8 @@ import 'package:coconut_wallet/utils/logger.dart';
 import 'package:coconut_wallet/utils/utxo_util.dart';
 
 typedef CpfpInfo = ({
-  String parentTransactionHash, // 부모 트랜잭션
-  double originalFee, // 원본 수수료율
+  String parentTransactionHash,
+  double originalFee,
   List<Transaction> previousTransactions,
 });
 
@@ -25,14 +26,35 @@ class CpfpService {
     this._electrumService,
   );
 
-  /// CPFP 트랜잭션을 감지합니다.
-  ///
-  /// CPFP는 미확인 트랜잭션의 출력을 입력으로 사용하는 새로운 트랜잭션이 발생했을 때 감지됩니다.
+  /// 기존 CPFP 내역이 있는지 확인
+  bool hasExistingCpfpHistory(int walletId, String txHash) {
+    final existingCpfpHistory = _transactionRepository.getCpfpHistory(walletId, txHash);
+    return existingCpfpHistory != null;
+  }
+
+  /// CPFP 대상이 되는 UTXO를 찾는 함수
+  Future<UtxoState?> findCpfpCandidate(int walletId, Transaction tx) async {
+    for (final input in tx.inputs) {
+      final utxoId = makeUtxoId(input.transactionHash, input.index);
+      final utxo = _utxoRepository.getUtxoState(walletId, utxoId);
+
+      if (utxo != null) {
+        final parentTxRecord =
+            _transactionRepository.getTransactionRecord(walletId, input.transactionHash);
+        // 부모 트랜잭션이 미확인 상태인 경우 CPFP 후보
+        if (parentTxRecord != null && parentTxRecord.blockHeight == 0) {
+          return utxo;
+        }
+      }
+    }
+    return null;
+  }
+
+  /// CPFP 트랜잭션을 감지하는 함수
   Future<CpfpInfo?> detectCpfpTransaction(int walletId, Transaction tx) async {
     // 이미 CPFP 내역이 있는지 확인
-    final existingCpfpHistory = _transactionRepository.getCpfpHistory(walletId, tx.transactionHash);
-    if (existingCpfpHistory != null) {
-      return null; // 이미 CPFP로 등록된 트랜잭션
+    if (hasExistingCpfpHistory(walletId, tx.transactionHash)) {
+      return null;
     }
 
     // 트랜잭션의 입력이 미확인 트랜잭션의 출력을 사용하는지 확인
@@ -41,14 +63,12 @@ class CpfpService {
     double originalFee = 0.0;
 
     for (final input in tx.inputs) {
-      // 입력으로 사용된 트랜잭션이 미확인 상태인지 확인
       final parentTxRecord =
           _transactionRepository.getTransactionRecord(walletId, input.transactionHash);
-      // 입력 트랜잭션이 언컨펌이면서
+
       if (parentTxRecord != null && parentTxRecord.blockHeight == 0) {
         final utxo =
             _utxoRepository.getUtxoState(walletId, makeUtxoId(input.transactionHash, input.index));
-        // 입력 트랜잭션에 사용된 UTXO가 내 UTXO라면 CPFP로 간주
         if (utxo != null) {
           isCpfp = true;
           parentTxHash = parentTxRecord.transactionHash;
@@ -59,9 +79,7 @@ class CpfpService {
     }
 
     if (isCpfp && parentTxHash != null) {
-      // 수수료 계산을 위한 정보 수집
       final prevTxs = await _electrumService.getPreviousTransactions(tx);
-
       return (
         parentTransactionHash: parentTxHash,
         originalFee: originalFee,
@@ -72,8 +90,13 @@ class CpfpService {
     return null;
   }
 
-  Future<void> saveCpfpHistoryMap(WalletListItemBase walletItem, Map<String, CpfpInfo> cpfpInfoMap,
-      Map<String, TransactionRecord> txRecordMap, int walletId) async {
+  /// CPFP 내역을 저장하는 함수
+  Future<void> saveCpfpHistoryMap(
+    WalletListItemBase walletItem,
+    Map<String, CpfpInfo> cpfpInfoMap,
+    Map<String, TransactionRecord> txRecordMap,
+    int walletId,
+  ) async {
     final cpfpHistoryDtos = <CpfpHistoryDto>[];
 
     for (final entry in cpfpInfoMap.entries) {
---
+++ test/providers/node_provider/transaction/cpfp_service_test.dart
@@ -0,0 +1,196 @@
+import 'package:coconut_lib/coconut_lib.dart';
+import 'package:coconut_wallet/model/wallet/watch_only_wallet.dart';
+import 'package:coconut_wallet/providers/node_provider/transaction/cpfp_service.dart';
+import 'package:coconut_wallet/repository/realm/transaction_repository.dart';
+import 'package:coconut_wallet/repository/realm/utxo_repository.dart';
+import 'package:coconut_wallet/repository/realm/wallet_repository.dart';
+import 'package:coconut_wallet/repository/shared_preference/shared_prefs_repository.dart';
+import 'package:coconut_wallet/services/electrum_service.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../../mock/transaction_mock.dart';
+import '../../../mock/utxo_mock.dart';
+import '../../../mock/wallet_mock.dart';
+import '../../../repository/realm/test_realm_manager.dart';
+import '../../../services/shared_prefs_service_test.mocks.dart';
+import 'rbf_service_test.mocks.dart';
+import 'package:coconut_wallet/model/wallet/transaction_record.dart';
+
+@GenerateMocks([
+  ElectrumService,
+])
+void main() {
+  group('CpfpService', () {
+    TestRealmManager? realmManager;
+    late TransactionRepository transactionRepository;
+    late WalletRepository walletRepository;
+    late UtxoRepository utxoRepository;
+    late MockElectrumService electrumService;
+    late CpfpService cpfpService;
+
+    const int walletId = 1;
+    final walletItem = WalletMock.createSingleSigWalletItem(id: walletId);
+    final parentTx = TransactionMock.createMockTransaction(
+      toAddress: walletItem.walletBase.getAddress(0),
+      amount: 10000,
+    );
+    final childTx = TransactionMock.createMockTransaction(
+      toAddress: walletItem.walletBase.getAddress(1),
+      amount: 9000,
+      inputTransactionHash: parentTx.transactionHash,
+    );
+
+    setUp(() async {
+      if (realmManager == null) {
+        realmManager = await setupTestRealmManager();
+      } else {
+        realmManager!.dispose();
+        realmManager = await setupTestRealmManager();
+      }
+      final sharedPrefsRepository = SharedPrefsRepository()
+        ..setSharedPreferencesForTest(MockSharedPreferences());
+      when(sharedPrefsRepository.getInt('nextId')).thenReturn(walletItem.id);
+      when(sharedPrefsRepository.setInt('nextId', walletItem.id + 1)).thenAnswer((_) async => true);
+      transactionRepository = TransactionRepository(realmManager!);
+      utxoRepository = UtxoRepository(realmManager!);
+      walletRepository = WalletRepository(realmManager!);
+      electrumService = MockElectrumService();
+      cpfpService = CpfpService(transactionRepository, utxoRepository, electrumService);
+      walletRepository.addSinglesigWallet(WatchOnlyWallet(walletItem.name, walletItem.colorIndex,
+          walletItem.iconIndex, walletItem.descriptor, null, null));
+    });
+
+    tearDown(() {
+      realmManager?.dispose();
+      realmManager = null;
+    });
+
+    group('hasExistingCpfpHistory', () {
+      test('기존 CPFP 내역이 있을 경우 true 반환', () async {
+        // Given
+        final cpfpHistory = CpfpHistoryDto(
+          walletId: walletId,
+          parentTransactionHash: parentTx.transactionHash,
+          childTransactionHash: childTx.transactionHash,
+          originalFee: 1.0,
+          newFee: 2.0,
+          timestamp: DateTime.now(),
+        );
+        transactionRepository.addAllCpfpHistory([cpfpHistory]);
+
+        // When
+        final result = cpfpService.hasExistingCpfpHistory(walletId, childTx.transactionHash);
+
+        // Then
+        expect(result, true);
+      });
+
+      test('기존 CPFP 내역이 없을 경우 false 반환', () async {
+        // When: 아무 내역도 없는 상태에서 조회
+        final result = cpfpService.hasExistingCpfpHistory(walletId, childTx.transactionHash);
+
+        // Then: false 반환
+        expect(result, false);
+      });
+    });
+
+    group('findCpfpCandidate', () {
+      test('CPFP 후보가 있을 경우 해당 UTXO 반환', () async {
+        // Given
+        final utxo = UtxoMock.createOutgoingUtxo(
+          walletId: walletId,
+          transactionHash: parentTx.transactionHash,
+          index: 0,
+        );
+        utxoRepository.addAllUtxos(walletId, [utxo]);
+        final parentTxRecord = TransactionMock.createUnconfirmedTransactionRecord(
+          transactionHash: parentTx.transactionHash,
+        );
+        await transactionRepository.addAllTransactions(walletId, [parentTxRecord]);
+
+        // When
+        final result = await cpfpService.findCpfpCandidate(walletId, childTx);
+
+        // Then
+        expect(result, isNotNull);
+        expect(result?.transactionHash, parentTx.transactionHash);
+      });
+
+      test('CPFP 후보가 없을 경우 null 반환', () async {
+        // When
+        final result = await cpfpService.findCpfpCandidate(walletId, childTx);
+
+        // Then
+        expect(result, isNull);
+      });
+    });
+
+    group('detectCpfpTransaction', () {
+      test('CPFP 트랜잭션 감지 성공', () async {
+        // Given
+        final utxo = UtxoMock.createOutgoingUtxo(
+          walletId: walletId,
+          transactionHash: parentTx.transactionHash,
+          index: 0,
+        );
+        utxoRepository.addAllUtxos(walletId, [utxo]);
+        final parentTxRecord = TransactionMock.createUnconfirmedTransactionRecord(
+          transactionHash: parentTx.transactionHash,
+          fee: 1000,
+        );
+        await transactionRepository.addAllTransactions(walletId, [parentTxRecord]);
+        when(electrumService.getPreviousTransactions(childTx)).thenAnswer((_) async => [parentTx]);
+
+        // When
+        final result = await cpfpService.detectCpfpTransaction(walletId, childTx);
+
+        // Then
+        expect(result, isNotNull);
+        expect(result?.parentTransactionHash, parentTx.transactionHash);
+        expect(result?.originalFee, parentTxRecord.feeRate);
+        expect(result?.previousTransactions, isA<List<Transaction>>());
+      });
+
+      test('이미 CPFP 내역이 있는 경우 null 반환', () async {
+        // Given
+        final cpfpHistory = CpfpHistoryDto(
+          walletId: walletId,
+          parentTransactionHash: parentTx.transactionHash,
+          childTransactionHash: childTx.transactionHash,
+          originalFee: 1.0,
+          newFee: 2.0,
+          timestamp: DateTime.now(),
+        );
+        transactionRepository.addAllCpfpHistory([cpfpHistory]);
+
+        // When
+        final result = await cpfpService.detectCpfpTransaction(walletId, childTx);
+
+        // Then
+        expect(result, isNull);
+      });
+
+      test('부모 트랜잭션이 미확인 상태가 아닌 경우 null 반환', () async {
+        // Given
+        final utxo = UtxoMock.createOutgoingUtxo(
+          walletId: walletId,
+          transactionHash: parentTx.transactionHash,
+          index: 0,
+        );
+        utxoRepository.addAllUtxos(walletId, [utxo]);
+        final parentTxRecord = TransactionMock.createConfirmedTransactionRecord(
+          transactionHash: parentTx.transactionHash,
+        );
+        await transactionRepository.addAllTransactions(walletId, [parentTxRecord]);
+
+        // When
+        final result = await cpfpService.detectCpfpTransaction(walletId, childTx);
+
+        // Then
+        expect(result, isNull);
+      });
+    });
+  });
+}
