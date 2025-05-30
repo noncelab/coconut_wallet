ed610dc0a0dfa07a4a3119870d1b6b23c68b9321
Yumin Kim <luke@noncelab.com>
2025-05-30 10:19:53 +0900

    fix(ln_invoice_service): json의 값이 제대로 들어오지 않는 경우 예외처리

--- lib/services/speed_app_ln_invoice_service.dart
+++ lib/services/speed_app_ln_invoice_service.dart
@@ -4,19 +4,25 @@ import 'package:dio/dio.dart';
 class SpeedAppLnInvoiceService {
   final _dio = Dio();
 
-  Future<dynamic> getLnInvoiceOfPow(int amount) async {
+  Future<String> getLnInvoiceOfPow(int amount) async {
     try {
       final speedAppResponse = await _dio.get('https://speed.app/.well-known/lnurlp/powbitcoiner');
+      if (speedAppResponse.data['callback'] == null) {
+        throw Exception("callback is null");
+      }
+
       final lnInvoiceResponse =
           await _dio.get("${speedAppResponse.data['callback']}?amount=$amount");
       if (lnInvoiceResponse.data['status'] == "ERROR") {
-        throw Exception(lnInvoiceResponse.data['reason']);
+        throw Exception(lnInvoiceResponse.data['reason'] ?? "status is ERROR");
+      } else if (lnInvoiceResponse.data['pr'] == null) {
+        throw Exception("pr is null");
       }
 
       return lnInvoiceResponse.data['pr'];
     } catch (e) {
       Logger.log("[ERROR] : $e");
-      throw Exception(e);
+      rethrow;
     }
   }
 }
