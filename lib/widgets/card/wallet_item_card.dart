1259bb24a451d638e1dfcb1eb717219f73f17fa3
pio <pio@noncelab.com>
2025-06-27 19:08:03 +0900

    feat(wallet-list): 홈화면 UI 개편 - 머지 충돌 제거

--- lib/widgets/card/wallet_item_card.dart
+++ lib/widgets/card/wallet_item_card.dart
@@ -4,11 +4,13 @@ import 'package:coconut_wallet/enums/wallet_enums.dart';
 import 'package:coconut_wallet/localization/strings.g.dart';
 import 'package:coconut_wallet/model/wallet/balance.dart';
 import 'package:coconut_wallet/model/wallet/multisig_signer.dart';
+import 'package:coconut_wallet/utils/balance_format_util.dart';
 import 'package:coconut_wallet/utils/colors_util.dart';
 import 'package:coconut_wallet/widgets/animated_balance.dart';
 import 'package:coconut_wallet/widgets/icon/wallet_item_icon.dart';
 import 'package:flutter/material.dart';
 import 'package:coconut_wallet/widgets/button/shrink_animation_button.dart';
+import 'package:flutter_svg/svg.dart';
 
 class WalletItemCard extends StatelessWidget {
   /// External Wallet인 경우 iconIndex = colorIndex = null
@@ -41,70 +43,104 @@ class WalletItemCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final isExternalWallet = walletImportSource != WalletImportSource.coconutVault;
-    final row = Padding(
-      padding: const EdgeInsets.symmetric(horizontal: CoconutLayout.defaultPadding),
-      child: ShrinkAnimationButton(
-          defaultColor: isExternalWallet ? CoconutColors.gray900 : CoconutColors.gray800,
-          onPressed: () {
-            Navigator.pushNamed(context, '/wallet-detail', arguments: {'id': id});
-          },
-          // Coconut Vault에서 가져온 멀티시그 지갑 => 테두리 그라디언트 적용
-          // External Wallet에서 가져온 싱글시그 지갑 => 테두리 CoconutColors.gray800
-          // Coconut Vault에서 가져온 싱글시그 지갑 => 테두리 없음(배경색과 동일)
-          borderGradientColors: signers?.isNotEmpty == true
-              ? ColorUtil.getGradientColors(signers!)
-              : [CoconutColors.gray800, CoconutColors.gray800],
-          pressedColor: isExternalWallet ? CoconutColors.black : CoconutColors.gray900,
-          child: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 24.0),
-              child: Row(children: [
-                WalletItemIcon(
-                    walletImportSource: walletImportSource,
-                    iconIndex: iconIndex,
-                    colorIndex: colorIndex),
-                CoconutLayout.spacing_200w,
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        name,
-                        style: CoconutTypography.body3_12.setColor(CoconutColors.gray500),
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      CoconutLayout.spacing_50h,
-                      isBalanceHidden
-                          ? Container(
-                              width: 100,
-                              height: 24,
-                              decoration: BoxDecoration(
-                                color: CoconutColors.white.withOpacity(0.1),
-                                borderRadius: BorderRadius.circular(4),
+    final displayFakeBalance = fakeBlance != null
+        ? currentUnit == BitcoinUnit.btc
+            ? satoshiToBitcoinString(fakeBlance!)
+            : addCommasToIntegerPart(fakeBlance!.toDouble())
+        : '';
+    // final isExternalWallet = walletImportSource != WalletImportSource.coconutVault;
+    final row = ShrinkAnimationButton(
+      defaultColor: CoconutColors.gray800,
+      pressedColor: CoconutColors.gray750,
+      borderRadius: 12,
+      onPressed: () {
+        Navigator.pushNamed(context, '/wallet-detail', arguments: {'id': id});
+      },
+      // ** gradient 사용안할수도 있음**
+      // Coconut Vault에서 가져온 멀티시그 지갑 => 테두리 그라디언트 적용
+      // External Wallet에서 가져온 싱글시그 지갑 => 테두리 CoconutColors.gray800
+      // Coconut Vault에서 가져온 싱글시그 지갑 => 테두리 없음(배경색과 동일)
+      // borderGradientColors: signers?.isNotEmpty == true
+      //     ? ColorUtil.getGradientColors(signers!)
+      //     : [CoconutColors.gray800, CoconutColors.gray800],
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 12.0),
+        child: Row(
+          children: [
+            WalletItemIcon(
+                walletImportSource: walletImportSource,
+                iconIndex: iconIndex,
+                colorIndex: colorIndex),
+            CoconutLayout.spacing_200w,
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  isBalanceHidden
+                      ? fakeBlance != null
+                          ? FittedBox(
+                              fit: BoxFit.scaleDown,
+                              alignment: Alignment.centerLeft,
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    displayFakeBalance,
+                                    style: CoconutTypography.body2_14_NumberBold
+                                        .setColor(CoconutColors.white),
+                                  ),
+                                  Text(
+                                    " ${currentUnit == BitcoinUnit.btc ? t.btc : t.sats}",
+                                    style: CoconutTypography.body2_14_NumberBold
+                                        .setColor(CoconutColors.white),
+                                  ),
+                                ],
                               ),
                             )
-                          : Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
-                              AnimatedBalance(
-                                  prevValue: animatedBalanceData.previous,
-                                  value: animatedBalanceData.current,
-                                  isBtcUnit: currentUnit == BitcoinUnit.btc,
-                                  textStyle: CoconutTypography.heading3_21_NumberBold
-                                      .setColor(CoconutColors.white)),
-                              Text(
-                                " ${currentUnit == BitcoinUnit.btc ? t.btc : t.sats}",
-                                style: CoconutTypography.body3_12_Number.copyWith(
-                                    color: CoconutColors.gray500, fontWeight: FontWeight.w500),
-                              ),
-                            ]),
-                    ],
+                          : Text(
+                              t.view_balance,
+                              style: CoconutTypography.body2_14_Bold
+                                  .copyWith(color: CoconutColors.gray600),
+                            )
+                      : FittedBox(
+                          fit: BoxFit.scaleDown,
+                          alignment: Alignment.centerLeft,
+                          child: Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
+                            AnimatedBalance(
+                                prevValue: animatedBalanceData.previous,
+                                value: animatedBalanceData.current,
+                                isBtcUnit: currentUnit == BitcoinUnit.btc,
+                                textStyle: CoconutTypography.body2_14_NumberBold
+                                    .setColor(CoconutColors.white)),
+                            Text(
+                              " ${currentUnit == BitcoinUnit.btc ? t.btc : t.sats}",
+                              style: CoconutTypography.body2_14_NumberBold
+                                  .setColor(CoconutColors.white),
+                            ),
+                          ]),
+                        ),
+                  Text(
+                    name,
+                    style: CoconutTypography.body3_12.setColor(CoconutColors.gray500),
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
                   ),
-                ),
-                CoconutLayout.spacing_200w,
-                // SvgPicture.asset('assets/svg/arrow-right.svg',
-                //     width: 24,
-                //     colorFilter: const ColorFilter.mode(CoconutColors.white, BlendMode.srcIn))
-              ]))),
+                ],
+              ),
+            ),
+            CoconutLayout.spacing_200w,
+            SvgPicture.asset(
+              'assets/svg/arrow-right.svg',
+              width: 6,
+              height: 10,
+              colorFilter: const ColorFilter.mode(
+                CoconutColors.gray400,
+                BlendMode.srcIn,
+              ),
+            )
+          ],
+        ),
+      ),
     );
 
     if (isLastItem) {
