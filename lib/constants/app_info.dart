c76a46fc11891c666fdb967618eb4f7a78bc92eb
ella <eunji@noncelab.com>
2025-07-16 19:08:32 +0900

    deploy(0.0.14): beta

--- lib/constants/app_info.dart
+++ lib/constants/app_info.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: constant_identifier_names
 
 /// TODO: release date 기록
-const RELEASE_DATE = '2025.07.14';
+const RELEASE_DATE = '2025.07.16';
 const COPYRIGHT_TEXT =
     '© 2025 Nonce Lab. Inc. in S.Korea.\nLicensed under the MIT License with Commons Clause.';
 
