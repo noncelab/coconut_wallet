8abd3dbeb8e2c61411b4d9d688add44b6b118c5c
pio <pio@noncelab.com>
2025-07-18 10:38:29 +0900

    fix(survey-bottom-sheet): 개행 되는 문자를 fittedBox를 이용해 한줄 표시, 패딩 적용

--- lib/screens/review/negative_feedback_screen.dart
+++ lib/screens/review/negative_feedback_screen.dart
@@ -34,60 +34,66 @@ class NegativeFeedbackScreen extends StatelessWidget {
         ),
         backgroundColor: CoconutColors.black,
         body: SafeArea(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  t.negative_feedback_screen.text1,
-                  style: CoconutTypography.heading2_28_NumberBold.setColor(CoconutColors.white),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Text(
-                  t.negative_feedback_screen.text2,
-                  style: CoconutTypography.body1_16,
-                ),
-                const SizedBox(
-                  height: 80,
-                ),
-                GestureDetector(
-                  onTap: () => _runKakaoOpenChat(context),
-                  child: Container(
-                      width: MediaQuery.of(context).size.width * 0.5,
-                      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(14), color: CoconutColors.primary),
-                      child: Text(
-                        t.negative_feedback_screen.text3,
-                        style: CoconutTypography.body2_14_Bold.setColor(CoconutColors.gray800),
-                        textAlign: TextAlign.center,
-                      )),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                GestureDetector(
-                  onTap: () => _stopGettingFeedback(context),
-                  child: Container(
-                      width: MediaQuery.of(context).size.width * 0.5,
-                      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(14),
-                        color: CoconutColors.white.withOpacity(0.15),
-                      ),
-                      child: Text(
-                        t.negative_feedback_screen.text4,
-                        style: CoconutTypography.body2_14_Bold.setColor(CoconutColors.white),
-                        textAlign: TextAlign.center,
-                      )),
-                ),
-                const SizedBox(
-                  height: 40,
-                ),
-              ],
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    t.negative_feedback_screen.text1,
+                    style: CoconutTypography.heading2_28_NumberBold.setColor(CoconutColors.white),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  FittedBox(
+                    child: Text(
+                      t.negative_feedback_screen.text2,
+                      style: CoconutTypography.body1_16,
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 80,
+                  ),
+                  GestureDetector(
+                    onTap: () => _runKakaoOpenChat(context),
+                    child: Container(
+                        width: MediaQuery.of(context).size.width * 0.5,
+                        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(14), color: CoconutColors.primary),
+                        child: Text(
+                          t.negative_feedback_screen.text3,
+                          style: CoconutTypography.body2_14_Bold.setColor(CoconutColors.gray800),
+                          textAlign: TextAlign.center,
+                        )),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  GestureDetector(
+                    onTap: () => _stopGettingFeedback(context),
+                    child: Container(
+                        width: MediaQuery.of(context).size.width * 0.5,
+                        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(14),
+                          color: CoconutColors.white.withOpacity(0.15),
+                        ),
+                        child: Text(
+                          t.negative_feedback_screen.text4,
+                          style: CoconutTypography.body2_14_Bold.setColor(CoconutColors.white),
+                          textAlign: TextAlign.center,
+                        )),
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                ],
+              ),
             ),
           ),
         ),
