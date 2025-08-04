f3b623243fe6aae08892699505e354a4e5962ab7
ella <eunji@noncelab.com>
2025-08-04 14:44:36 +0900

    fix(isolate_constants): kIsolateInitTimeout 3 -> 10초

--- lib/constants/isolate_constants.dart
+++ lib/constants/isolate_constants.dart
@@ -1,4 +1,4 @@
-const Duration kIsolateInitTimeout = Duration(seconds: 3);
+const Duration kIsolateInitTimeout = Duration(seconds: 10);
 const Duration kIsolateSocketCheckTimeout = Duration(milliseconds: 100);
 const Duration kIsolateResponseTimeout = Duration(seconds: 180);
 const Duration kIsolateSimpleResponseTimeout = Duration(seconds: 3);
