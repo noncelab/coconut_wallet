ec4c9ee1874f44e9b6ef302d665d38d25afaa7c7
doey <doey@noncelab.com>
2025-08-05 11:41:40 +0900

    feat(onion): onion 접속인 경우 timeout을 30s로 늘림

--- lib/constants/isolate_constants.dart
+++ lib/constants/isolate_constants.dart
@@ -1,4 +1,6 @@
-const Duration kIsolateInitTimeout = Duration(seconds: 10);
+const Duration kIsolateInitTimeout = Duration(seconds: 3);
+const Duration kIsolateInitTimeoutForOnion = Duration(seconds: 30);
 const Duration kIsolateSocketCheckTimeout = Duration(milliseconds: 100);
 const Duration kIsolateResponseTimeout = Duration(seconds: 180);
-const Duration kIsolateSimpleResponseTimeout = Duration(seconds: 10);
+const Duration kIsolateSimpleResponseTimeout = Duration(seconds: 3);
+const Duration kIsolateSimpleResponseTimeoutForOnion = Duration(seconds: 30);
