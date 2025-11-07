59110af3b4726f3b218b5a542dd4d4094bface12
pio <pio@noncelab.com>
2025-11-07 11:12:46 +0900

    style(vibration-comment): Vibration Util 각 메서드별로 코멘트 추가

--- lib/utils/vibration_util.dart
+++ lib/utils/vibration_util.dart
@@ -3,6 +3,8 @@ import 'dart:io';
 import 'package:flutter/services.dart';
 import 'package:vibration/vibration.dart';
 
+/// 매우 가벼운 피드백이 필요할 때 사용 (예: 버튼 하이라이트, 토글 온/오프)
+/// 사용 중: PIN 번호 입력 시, 언어 선택 시 항목 탭, 스캔 화면에서 Density 조절 시, ...
 void vibrateExtraLight() {
   if (Platform.isAndroid) {
     Vibration.vibrate(duration: 10, amplitude: 170);
@@ -11,6 +13,7 @@ void vibrateExtraLight() {
   }
 }
 
+/// 연속된 가벼운 피드백이 필요한 상황에 사용 (현재 사용중인 곳 없음)
 void vibrateExtraLightDouble() async {
   if (Platform.isAndroid) {
     Vibration.vibrate(pattern: [0, 10, 0, 10]);
@@ -21,7 +24,7 @@ void vibrateExtraLightDouble() async {
   }
 }
 
-// 안드로이드에서는 경고 또는 알림용 진동이 필요하지 않는 경우 vibrateExtraLight 사용
+/// 일반적인 피드백이 필요할 때 사용 (예: 일반 성공/실패 안내)
 void vibrateLight() {
   if (Platform.isAndroid) {
     Vibration.vibrate(duration: 100, amplitude: 170);
@@ -30,6 +33,7 @@ void vibrateLight() {
   }
 }
 
+/// 강한 경고나 주의가 필요한 상황에 사용 (예: 오류 알림, 중요한 안내)
 void vibrateMedium() {
   if (Platform.isAndroid) {
     Vibration.vibrate(duration: 1000);
@@ -38,6 +42,7 @@ void vibrateMedium() {
   }
 }
 
+/// 실패를 알리기 위한 피드백에 사용 (예: 지갑 추가 실패, PIN 검증 실패, ...)
 void vibrateLightDouble() async {
   if (Platform.isAndroid) {
     Vibration.vibrate(pattern: [0, 100, 100, 100], amplitude: 170);
