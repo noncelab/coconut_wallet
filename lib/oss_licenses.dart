5c1cdb328d18b0f3e4839e66088603400c24ab12
ella <eunji@noncelab.com>
2026-01-09 10:45:50 +0900

    deploy: regtest 3.5.1

--- lib/oss_licenses.dart
+++ lib/oss_licenses.dart
@@ -368,51 +368,6 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.''',
   dependencies: [PackageRef('meta')],
 );
 
-/// _flutterfire_internals 1.3.59
-const __flutterfire_internals = Package(
-  name: '_flutterfire_internals',
-  description: 'A package hosting Dart code shared between FlutterFire plugins.',
-  homepage: 'https://firebase.google.com/docs/firestore',
-  repository: 'https://github.com/firebase/flutterfire/tree/main/packages/_flutterfire_internals',
-  authors: [],
-  version: '1.3.59',
-  license: '''Copyright 2017, the Chromium project authors. All rights reserved.
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are
-met:
-
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above
-      copyright notice, this list of conditions and the following
-      disclaimer in the documentation and/or other materials provided
-      with the distribution.
-    * Neither the name of Google Inc. nor the names of its
-      contributors may be used to endorse or promote products derived
-      from this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.''',
-  isMarkdown: false,
-  isSdk: false,
-  dependencies: [
-    PackageRef('collection'),
-    PackageRef('firebase_core'),
-    PackageRef('firebase_core_platform_interface'),
-    PackageRef('flutter'),
-    PackageRef('meta'),
-  ],
-);
-
 /// analyzer 7.7.1
 const _analyzer = Package(
   name: 'analyzer',
@@ -10187,13 +10142,13 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.''',
   dependencies: [],
 );
 
-/// tar 2.0.1
+/// tar 2.0.2
 const _tar = Package(
   name: 'tar',
   description: 'Memory-efficient, streaming implementation of the tar file format',
   repository: 'https://github.com/simolus3/tar/',
   authors: [],
-  version: '2.0.1',
+  version: '2.0.2',
   license: '''MIT License
 
 Copyright (c) 2021 Simon Binder
