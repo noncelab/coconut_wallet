7bceff59f778a67a22d3ac42b7b9f3f6cf51af5e
ella <eunji@noncelab.com>
2026-04-01 16:38:56 +0900

    feat(UtxoSplitBuilder): niceAmounts 선언

--- lib/core/transaction/utxo_split_builder.dart
+++ lib/core/transaction/utxo_split_builder.dart
@@ -54,6 +54,26 @@ class UtxoSplitBuilder {
   late final double _lastAmountMargin;
   double? _outputVBytes;
   double? _oneOutputTxVBytes;
+  static const List<double> niceAmounts = [
+    0.0001,
+    0.0002,
+    0.0005,
+    0.001,
+    0.002,
+    0.005,
+    0.01,
+    0.02,
+    0.05,
+    0.1,
+    0.2,
+    0.5,
+    1,
+    2,
+    5,
+    10,
+    20,
+    50,
+  ];
 
   double get feeRate => _feeRate;
   set feeRate(double value) {
